202981073fbbb31b88efc6223c31fb2be16b605b
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-09-18 13:33:13 +0100

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Project1 {
+  public static void main(String[] args) {}
+
+
+  String snacksAsString, name, className;
+ int snacks, total;
+
+         name = JOptionPane.showInputDialog("Please enter your name");
+         snacksAsString = JOptionPane.showInputDialog("Please enter the amount of snacks you want");
+
+  snacks = Integer.parseInt(snacksAsString);
+  snacks * 2  = total
+
+
+JOptionPane.showMessageDialog(null,"The total cost is " + total + );
+
+}
